2c421d4bf7a849026a863a3055b52b6e5618362d
Cristian Pop <cristian.pop@analog.com>
2020-12-10 15:05:52 +0200

    drivers/adc: Add ad9083 initial driver
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>

---
+++ projects/ad9083/src.mk
@@ -0,0 +1,93 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+SRCS += $(PROJECT)/src/main.c
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(PROJECT)/src/app_iio.c
+LIBRARIES += iio
+endif
+SRCS += $(DRIVERS)/spi/spi.c						\
+	$(DRIVERS)/gpio/gpio.c						\
+	$(DRIVERS)/frequency/hmc7044/hmc7044.c				\
+	$(DRIVERS)/adc/ad9083/ad9083.c 					\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_device.c			\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_hal.c			\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_jesd.c			\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_rx.c
+SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c			\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c		\
+	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c			\
+	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
+	$(NO-OS)/util/util.c
+SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
+	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c				\
+	$(PLATFORM_DRIVERS)/delay.c
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(NO-OS)/util/xml.c						\
+	$(NO-OS)/util/fifo.c						\
+	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.c				\
+	$(NO-OS)/util/list.c						\
+	$(PLATFORM_DRIVERS)/uart.c					\
+	$(PLATFORM_DRIVERS)/irq.c
+endif
+INCS += $(PROJECT)/src/parameters.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS +=	$(PROJECT)/src/app_iio.h
+endif
+INCS += $(DRIVERS)/frequency/hmc7044/hmc7044.h				\
+	$(DRIVERS)/adc/ad9083/ad9083.h					\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_datapath.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_dig_scan.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_eng.h			\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_g_h_sync.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_jtx_ip.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_jtx_qbf.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_lcpll_28nm_r1.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_ser_phy.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_spi.h			\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_top_clk_reg.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_top_ref_reg.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_vcoadc_lut.h		\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_bf_vcoadc_mem_map.h	\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_config.h			\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083.h				\
+	$(DRIVERS)/adc/ad9083/api/adi_ad9083_hal.h			\
+	$(DRIVERS)/adc/ad9083/api/adi_cms_api_common.h			\
+	$(DRIVERS)/adc/ad9083/api/adi_cms_api_config.h
+INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h		\
+	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h			\
+	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h
+INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
+	$(PLATFORM_DRIVERS)/gpio_extra.h
+INCS +=	$(INCLUDE)/axi_io.h						\
+	$(INCLUDE)/spi.h						\
+	$(INCLUDE)/gpio.h						\
+	$(INCLUDE)/error.h						\
+	$(INCLUDE)/delay.h						\
+	$(INCLUDE)/util.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(INCLUDE)/xml.h						\
+	$(INCLUDE)/fifo.h						\
+	$(INCLUDE)/irq.h						\
+	$(INCLUDE)/uart.h						\
+	$(INCLUDE)/list.h						\
+	$(PLATFORM_DRIVERS)/irq_extra.h					\
+	$(PLATFORM_DRIVERS)/uart_extra.h                                \
+	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.h
+endif
